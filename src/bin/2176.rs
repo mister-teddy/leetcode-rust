36c919e374d7657da30835bd7f6960518acae582
PhatNH4 <phatnh4@vng.com.vn>
2025-04-17 10:54:51 +0700

    2176

---
+++ src/bin/2176.rs
@@ -0,0 +1,55 @@
+/// Category: algorithms
+/// Level: Easy
+/// Percent: 79.46446%
+
+/// Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.
+///
+/// Example 1:
+///
+/// Input: nums = [3,1,2,2,2,1,3], k = 2
+/// Output: 4
+/// Explanation:
+/// There are 4 pairs that meet all the requirements:
+/// - nums[0] == nums[6], and 0 * 6 == 0, which is divisible by 2.
+/// - nums[2] == nums[3], and 2 * 3 == 6, which is divisible by 2.
+/// - nums[2] == nums[4], and 2 * 4 == 8, which is divisible by 2.
+/// - nums[3] == nums[4], and 3 * 4 == 12, which is divisible by 2.
+///
+///
+/// Example 2:
+///
+/// Input: nums = [1,2,3,4], k = 1
+/// Output: 0
+/// Explanation: Since no value in nums is repeated, there are no pairs (i,j) that meet all the requirements.
+///
+///
+///
+/// Constraints:
+///
+///
+/// 	1 <= nums.length <= 100
+/// 	1 <= nums[i], k <= 100
+///
+
+impl Solution {
+    pub fn count_pairs(nums: Vec<i32>, k: i32) -> i32 {
+        let mut res = 0;
+        let k = k as usize;
+        for i in 1..nums.len() {
+            // j = ak/i
+            // j < i => ak/i < i => ak < i*i => a < i*i/k
+            let p = (i * i - 1) / k;
+            for a in 0..=p {
+                if a * k % i == 0 {
+                    let j = a * k / i;
+                    // print!("\ni*j = {i}*{j}");
+                    if nums[i] == nums[j] {
+                        // print!(" ✅");
+                        res += 1;
+                    }
+                }
+            }
+        }
+        res
+    }
+}
