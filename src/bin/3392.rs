b7b911d0d3afae4993fb6f312e4cda233ccb581b
Teddy <hongphat.js@gmail.com>
2025-04-27 12:39:11 +0700

    3392

---
+++ src/bin/3392.rs
@@ -0,0 +1,48 @@
+/// Category: algorithms
+/// Level: Easy
+/// Percent: 51.755142%
+
+/// Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
+///
+///
+/// Example 1:
+///
+///
+/// Input: nums = [1,2,1,4,1]
+///
+/// Output: 1
+///
+/// Explanation:
+///
+/// Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
+///
+///
+/// Example 2:
+///
+///
+/// Input: nums = [1,1,1]
+///
+/// Output: 0
+///
+/// Explanation:
+///
+/// [1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
+///
+///
+///
+/// Constraints:
+///
+///
+/// 	3 <= nums.length <= 100
+/// 	-100 <= nums[i] <= 100
+///
+
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>) -> i32 {
+        nums.iter()
+            .enumerate()
+            .skip(2)
+            .filter(|(i, x)| (*x + nums[i - 2]) * 2 == nums[i - 1])
+            .count() as i32
+    }
+}
