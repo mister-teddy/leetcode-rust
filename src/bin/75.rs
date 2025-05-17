3817002f1f6c874dfc8a098adfc72a904eb053a0
Teddy <hongphat.js@gmail.com>
2025-05-17 13:40:33 +0700

    75

---
+++ src/bin/75.rs
@@ -0,0 +1,50 @@
+/// Category: algorithms
+/// Level: Medium
+/// Percent: 66.72452%
+
+/// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+///
+/// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+///
+/// You must solve this problem without using the library's sort function.
+///
+///
+/// Example 1:
+///
+/// Input: nums = [2,0,2,1,1,0]
+/// Output: [0,0,1,1,2,2]
+///
+///
+/// Example 2:
+///
+/// Input: nums = [2,0,1]
+/// Output: [0,1,2]
+///
+///
+///
+/// Constraints:
+///
+///
+/// 	n == nums.length
+/// 	1 <= n <= 300
+/// 	nums[i] is either 0, 1, or 2.
+///
+///
+///
+/// Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+impl Solution {
+    pub fn sort_colors(nums: &mut Vec<i32>) {
+        let mut count = [0, 0, 0];
+        for num in nums.iter() {
+            count[*num as usize] += 1;
+        }
+        *nums = [0, 1, 2]
+            .map(|color| {
+                (0..count[color])
+                    .map(|_| color as i32)
+                    .collect::<Vec<i32>>()
+            })
+            .concat();
+    }
+}
